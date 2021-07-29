7650fc9352bfac6231442f755dba07f7f24a57cb
Alexandre Fonseca <alexandrefonseca@Alexandres-MacBook-Pro.local>
2021-07-29 11:08:07 -0300

    correcao botao footer

--- pages/simulator-02.js
+++ pages/simulator-02.js
@@ -211,8 +211,8 @@ export default function Simulator() {
         )}
       </Formik>
       </div>
-      <footer class="w-screen fixed bottom-0 flex">
-        <div className="bg-blue px-3 py-5 text-white flex items-center float-right">
+      <footer class="mr-1 mt-5">
+        <div className="bg-blue px-3 py-5 text-white flex items-center float-right rounded">
           <h3 className="inline mr-10">Continuar</h3>
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
