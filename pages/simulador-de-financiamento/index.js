81212ae865df72ff94bc68da6945c7e5ea9ad40b
Victor <victor@Victors-iMac-Pro.local>
2021-07-30 16:15:26 -0300

    pagina atualizada - faltando apenas a parte de apresentacao dos numeros

--- pages/simulador-de-financiamento/index.js
+++ pages/simulador-de-financiamento/index.js
@@ -148,33 +148,6 @@ export default function Home() {
           </Form>
         )}
       </Formik>
-      <table className="table-auto">
-        <thead>
-          <tr>
-            <th>Juros</th>
-            <th>Amortização</th>
-            <th>Parcela</th>
-            <th>Saldo devedor</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Intro to CSS</td>
-            <td>Adam</td>
-            <td>858</td>
-          </tr>
-          <tr>
-            <td>A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
-            <td>Adam</td>
-            <td>112</td>
-          </tr>
-          <tr>
-            <td>Intro to JavaScript</td>
-            <td>Chris</td>
-            <td>1,280</td>
-          </tr>
-        </tbody>
-      </table>
     </main>
   )
 }
